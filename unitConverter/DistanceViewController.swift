b4f2065ccba5668e2cec377d6b388cb21a2efef8
Nadeeka Athukorala <nadeekaathukorala@nadeekas-mac.local>
2018-08-19 07:00:15 -0700

    added distance and speed conversion

--- unitConverter/DistanceViewController.swift
+++ unitConverter/DistanceViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class DistanceViewController: UIViewController {
     
+    @IBOutlet var meterTextField: UITextField!
+    @IBOutlet var footTextField: UITextField!
+    @IBOutlet var yardTextfield: UITextField!
+    @IBOutlet var kmTextField: UITextField!
+    @IBOutlet var mileTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
@@ -20,6 +25,106 @@ class DistanceViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func meterTextField(_ sender: UITextField) {
+        let meterValue = Double(meterTextField.text!)
+        
+        if meterValue == nil {
+            footTextField.text = ""
+            yardTextfield.text = ""
+            kmTextField.text = ""
+            mileTextField.text = ""
+        }else{
+            let footValue = meterValue!*3.28084
+            let yardValue = meterValue!*1.09361
+            let kmValue = meterValue!*0.001
+            let mileValue = meterValue!*0.000621371
+            
+            footTextField.text = String(format:"%f",footValue)
+            yardTextfield.text = String(format:"%f",yardValue)
+            kmTextField.text = String(format:"%f",kmValue)
+            mileTextField.text = String(format:"%f",mileValue)
+        }
+    }
     
+    @IBAction func footTextField(_ sender: UITextField) {
+        let footValue = Double(footTextField.text!)
+        
+        if footValue == nil {
+            meterTextField.text = ""
+            yardTextfield.text = ""
+            kmTextField.text = ""
+            mileTextField.text = ""
+        }else{
+            let meterValue = footValue!*0.3048
+            let yardValue = footValue!*0.333333
+            let kmValue = footValue!*0.0003047996952
+            let mileValue = footValue!*0.00018939375000000000589
+            
+            meterTextField.text = String(format:"%f",meterValue)
+            yardTextfield.text = String(format:"%f",yardValue)
+            kmTextField.text = String(format:"%f",kmValue)
+            mileTextField.text = String(format:"%f",mileValue)
+        }
+    }
+    @IBAction func yardTextfield(_ sender: UITextField) {
+        let yardValue = Double(yardTextfield.text!)
+        
+        if yardValue == nil {
+            meterTextField.text = ""
+            footTextField.text = ""
+            kmTextField.text = ""
+            mileTextField.text = ""
+        }else{
+            let meterValue = yardValue!*0.9144
+            let footValue = yardValue!*3
+            let kmValue = yardValue!*0.0009144
+            let mileValue = yardValue!*0.000568182
+            
+            meterTextField.text = String(format:"%f",meterValue)
+            footTextField.text = String(format:"%f",footValue)
+            kmTextField.text = String(format:"%f",kmValue)
+            mileTextField.text = String(format:"%f",mileValue)
+        }
+    }
+    @IBAction func kmTextField(_ sender: UITextField) {
+        let kmValue = Double(kmTextField.text!)
+        
+        if kmValue == nil {
+            meterTextField.text = ""
+            footTextField.text = ""
+            yardTextfield.text = ""
+            mileTextField.text = ""
+        }else{
+            let meterValue = kmValue!*1000
+            let footValue = kmValue!*3280.84
+            let yardValue = kmValue!*1093.6133333333
+            let mileValue = kmValue!*0.621371
+            
+            meterTextField.text = String(format:"%f",meterValue)
+            footTextField.text = String(format:"%f",footValue)
+            yardTextfield.text = String(format:"%f",yardValue)
+            mileTextField.text = String(format:"%f",mileValue)
+        }
+    }
+    @IBAction func mileTextField(_ sender: UITextField) {
+        let mileValue = Double(mileTextField.text!)
+        
+        if mileValue == nil {
+            meterTextField.text = ""
+            footTextField.text = ""
+            yardTextfield.text = ""
+            kmTextField.text = ""
+        }else{
+            let meterValue = mileValue!*1609.34
+            let footValue = mileValue!*5280
+            let yardValue = mileValue!*1760
+            let kmValue = mileValue!*1.60934
+            
+            meterTextField.text = String(format:"%f",meterValue)
+            footTextField.text = String(format:"%f",footValue)
+            yardTextfield.text = String(format:"%f",yardValue)
+            kmTextField.text = String(format:"%f",kmValue)
+        }
+    }
 }
 
--- unitConverter/SpeedViewController.swift
+++ unitConverter/SpeedViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class SpeedViewController: UIViewController {
     
+    @IBOutlet var mpsTextField: UITextField!
+    @IBOutlet var feetpsTextField: UITextField!
+    @IBOutlet var kmphTextField: UITextField!
+    @IBOutlet var milesphTextfield: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
@@ -20,5 +24,76 @@ class SpeedViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func mpsTextField(_ sender: UITextField) {
+        let mpsValue = Double(mpsTextField.text!)
+        
+        if mpsValue == nil {
+            feetpsTextField.text = ""
+            kmphTextField.text = ""
+            milesphTextfield.text = ""
+        }else{
+            let feetpsValue = mpsValue!*3.28084
+            let kmphValue = mpsValue!*3.6
+            let milesphValue = mpsValue!*2.23694
+            
+            feetpsTextField.text = String(format:"%f",feetpsValue)
+            kmphTextField.text = String(format:"%f",kmphValue)
+            milesphTextfield.text = String(format:"%f",milesphValue)
+        }
+    }
+    
+    @IBAction func feetpsTextField(_ sender: UITextField) {
+        let feetpsValue = Double(feetpsTextField.text!)
+        
+        if feetpsValue == nil {
+            mpsTextField.text = ""
+            kmphTextField.text = ""
+            milesphTextfield.text = ""
+        }else{
+            let mpsValue = feetpsValue!*0.3048
+            let kmphValue = feetpsValue!*1.09728
+            let milesphValue = feetpsValue!*0.681818
+            
+            mpsTextField.text = String(format:"%f",mpsValue)
+            kmphTextField.text = String(format:"%f",kmphValue)
+            milesphTextfield.text = String(format:"%f",milesphValue)
+        }
+    }
+    
+    @IBAction func kmphTextField(_ sender: UITextField) {
+        let kmphValue = Double(kmphTextField.text!)
+        
+        if kmphValue == nil {
+            mpsTextField.text = ""
+            feetpsTextField.text = ""
+            milesphTextfield.text = ""
+        }else{
+            let mpsValue = kmphValue!*0.277778
+            let feetpsValue = kmphValue!*0.911344
+            let milesphValue = kmphValue!*0.621371
+            
+            mpsTextField.text = String(format:"%f",mpsValue)
+            feetpsTextField.text = String(format:"%f",feetpsValue)
+            milesphTextfield.text = String(format:"%f",milesphValue)
+        }
+    }
+    
+    @IBAction func milesphTextfield(_ sender: UITextField) {
+        let milesphValue = Double(milesphTextfield.text!)
+        
+        if milesphValue == nil {
+            mpsTextField.text = ""
+            feetpsTextField.text = ""
+            kmphTextField.text = ""
+        }else{
+            let mpsValue = milesphValue!*0.44704
+            let feetpsValue = milesphValue!*1.46667
+            let kmphValue = milesphValue!*1.60934
+            
+            mpsTextField.text = String(format:"%f",mpsValue)
+            feetpsTextField.text = String(format:"%f",feetpsValue)
+            kmphTextField.text = String(format:"%f",kmphValue)
+        }
+    }
     
 }
