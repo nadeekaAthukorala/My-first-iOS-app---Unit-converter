0572ddbdbee0fc9858400a9a017c3d561dd2a258
Nadeeka Athukorala <nadeekaathukorala@nadeekas-mac.local>
2018-08-19 01:45:13 -0700

    Weight conversion for grams added

--- unitConverter/WeightViewController.swift
+++ unitConverter/WeightViewController.swift
@@ -21,7 +21,11 @@ extension UIViewController {
 }
 
 class WeightViewController: UIViewController {
-
+    @IBOutlet var gramTextField: UITextField!
+    @IBOutlet var kilogramTextField: UITextField!
+    @IBOutlet var poundTextField: UITextField!
+    @IBOutlet var ounceTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
@@ -32,6 +36,23 @@ class WeightViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func gramTextField(_ sender: UITextField) {
+        let gramValue = Double(gramTextField.text!)
+        
+        if gramValue == nil {
+            kilogramTextField.text = ""
+            poundTextField.text = ""
+            ounceTextField.text = ""
+        }else{
+        let kilogramValue = gramValue!/1000
+        let poundValue = gramValue!*0.00220462
+        let ounceValue = gramValue!*0.035274
+        
+        kilogramTextField.text = String(format:"%f",kilogramValue)
+        poundTextField.text = String(format:"%f",poundValue)
+        ounceTextField.text = String(format:"%f",ounceValue)
+        }
+    }
+    
 }
 
