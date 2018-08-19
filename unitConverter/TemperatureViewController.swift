66db3d3d65349ac9344d8135e2e83602e7f02f6c
Nadeeka Athukorala <nadeekaathukorala@nadeekas-mac.local>
2018-08-19 04:05:33 -0700

    completed weight and temperature conversion functionalities

--- unitConverter/TemperatureViewController.swift
+++ unitConverter/TemperatureViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class TemperatureViewController: UIViewController {
 
+    @IBOutlet var celsiusTextField: UITextField!
+    @IBOutlet var farenheightTextField: UITextField!
+    @IBOutlet var kelvinTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
@@ -20,6 +24,49 @@ class TemperatureViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func celsiusTextField(_ sender: UITextField) {
+        let celsiusValue = Float32(celsiusTextField.text!)
+        
+        if celsiusValue == nil {
+            farenheightTextField.text = ""
+            kelvinTextField.text = ""
+        }else{
+            let farenheightValue = celsiusValue!*33.8
+            let kelvinValue = celsiusValue!*274.15
+            
+            farenheightTextField.text = String(format:"%f",farenheightValue)
+            kelvinTextField.text = String(format:"%f",kelvinValue)
+        }
+    }
+    
+    @IBAction func farenheightTextField(_ sender: UITextField) {
+        let farenheightValue = Float32(farenheightTextField.text!)
+        
+        if farenheightValue == nil {
+            celsiusTextField.text = ""
+            kelvinTextField.text = ""
+        }else{
+            let celsiusValue = -17.2222*farenheightValue!;
+            let kelvinValue = farenheightValue!*255.928
+            
+            celsiusTextField.text = String(format:"%f",celsiusValue)
+            kelvinTextField.text = String(format:"%f",kelvinValue)
+        }
+        
+    }
+    @IBAction func kelvinTextField(_ sender: UITextField) {
+        let kelvinValue = Float32(kelvinTextField.text!)
+        
+        if kelvinValue == nil {
+            celsiusTextField.text = ""
+            farenheightTextField.text = ""
+        }else{
+            let celsiusValue = -272.15*kelvinValue!;
+            let farenheightValue = -457.87*kelvinValue!;
+            
+            celsiusTextField.text = String(format:"%f",celsiusValue)
+            farenheightTextField.text = String(format:"%f",farenheightValue)
+        }
+    }
 }
 
--- unitConverter/WeightViewController.swift
+++ unitConverter/WeightViewController.swift
@@ -54,5 +54,60 @@ class WeightViewController: UIViewController {
         }
     }
     
+    @IBAction func kilogramTextField(_ sender: UITextField) {
+        let kilogramValue = Double(kilogramTextField.text!)
+        
+        if kilogramValue == nil {
+            gramTextField.text = ""
+            poundTextField.text = ""
+            ounceTextField.text = ""
+        }else{
+            let gramValue = kilogramValue!*1000
+            let poundValue = kilogramValue!*2.20462
+            let ounceValue = kilogramValue!*35.274
+            
+            gramTextField.text = String(format:"%f",gramValue)
+            poundTextField.text = String(format:"%f",poundValue)
+            ounceTextField.text = String(format:"%f",ounceValue)
+        }
+    }
+    
+    @IBAction func poundTextField(_ sender: UITextField) {
+        let poundValue = Double(poundTextField.text!)
+        
+        if poundValue == nil {
+            gramTextField.text = ""
+            kilogramTextField.text = ""
+            ounceTextField.text = ""
+        }else{
+            let gramValue = poundValue!*453.592
+            let kilogramValue = poundValue!*0.453592
+            let ounceValue = poundValue!*16
+            
+            gramTextField.text = String(format:"%f",gramValue)
+            kilogramTextField.text = String(format:"%f",kilogramValue)
+            ounceTextField.text = String(format:"%f",ounceValue)
+        }
+    }
+    
+    @IBAction func ounceTextField(_ sender: UITextField) {
+        let ounceValue = Double(ounceTextField.text!)
+        
+        if ounceValue == nil {
+            gramTextField.text = ""
+            kilogramTextField.text = ""
+            poundTextField.text = ""
+        }else{
+            let gramValue = ounceValue!*28.3495
+            let kilogramValue = ounceValue!*0.0283495
+            let poundValue = ounceValue!*0.0625
+            
+            gramTextField.text = String(format:"%f",gramValue)
+            kilogramTextField.text = String(format:"%f",kilogramValue)
+            poundTextField.text = String(format:"%f",poundValue)
+        }
+    }
+    
+    
 }
 
